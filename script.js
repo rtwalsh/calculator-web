8433aad7bf8d5e3f0eeeb2dd6b44c13b768d1da4
Robert Walsh <rwalsh@excalibur-solutions.com>
2024-08-27 18:19:21 -0400

    Add an operand variable and append each digit press to it

--- script.js
+++ script.js
@@ -5,10 +5,14 @@
  *  Date:   August 27, 2024
  */
 
+let operand;
+
 function initialize() {
+    operand = "";
     document.getElementById("display").textContent = "";
 }
 
 function digitPressed(digit) {
-    document.getElementById("display").textContent = digit;
+    operand = operand + digit; // could be operand += digit;
+    document.getElementById("display").textContent = operand;
 }
